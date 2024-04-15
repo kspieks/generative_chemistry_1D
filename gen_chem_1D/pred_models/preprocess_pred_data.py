62059d474d8aef302e1810a24bef07c827a4c4ba
Kevin Spiekermann <kspieker@mit.edu>
2024-04-15 18:41:38 -0400

    Add script for cleaning data before training predictive models

---
+++ gen_chem_1D/pred_models/preprocess_pred_data.py
@@ -0,0 +1,48 @@
+import argparse
+from pprint import pprint
+
+import pandas as pd
+
+from gen_chem_1D.data.data_cleaning import clean_smiles
+from gen_chem_1D.data.data_classes import Preprocess
+from gen_chem_1D.utils.parsing import read_yaml_file
+
+
+def clean_pred_data(preprocess_data_args):
+    """
+    Cleans data in preparation for training predictive model(s).
+
+    Args:
+        preprocess_data_args: dataclass storing arguments for preprocessing the SMILES.
+    """
+
+    # read in data as df
+    df = pd.read_csv(preprocess_data_args.pred_input_file)
+
+    # clean the SMILES
+    df = clean_smiles(df,
+                      min_heavy_atoms=preprocess_data_args.min_heavy_atoms,
+                      max_heavy_atoms=preprocess_data_args.max_heavy_atoms,
+                      supported_elements=preprocess_data_args.supported_elements,
+                      )
+    
+    df.to_csv(preprocess_data_args.pred_output_file, index=False)
+
+def main():
+    parser = argparse.ArgumentParser(description="Script to process data in preparation for training predictive model(s).")
+    parser.add_argument('--yaml_file', required=True,
+                        help='Path to yaml file containing arguments for preprocessing.')
+    args = parser.parse_args()
+
+    # read yaml file
+    print('Using arguments...')
+    yaml_dict = read_yaml_file(args.yaml_file)
+    pprint(yaml_dict)
+    print('\n\n')
+    
+    # clean and filter the data
+    preprocess_data_args = Preprocess(**yaml_dict['preprocess_data'])
+    clean_pred_data(preprocess_data_args)
+
+if __name__ == "__main__":
+    main()
