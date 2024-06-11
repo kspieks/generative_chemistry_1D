9b2c6e5938c61f6bca963e18cbbb6931998d96ed
Kevin Spiekermann <kspieker@mit.edu>
2024-06-11 18:15:39 -0400

    Vectorize the scoring functions, especially for making RF predictions
    This offers substantial speedup: approximately 40% faster based on local testing

--- gen_chem_1D/pred_models/scoring_functions.py
+++ gen_chem_1D/pred_models/scoring_functions.py
@@ -5,7 +5,9 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 
-from .features.featurizers import calc_atompair_fp, calc_donorpair_fp
+from gen_chem_1D.pred_models.features.create_features import create_features
+from gen_chem_1D.pred_models.rf_model import predict_rf
+
 
 class ConvertScore():
     """Class for converting a predicted or calculated value to a score bounded by 0 to 1"""
@@ -28,32 +30,31 @@ class ConvertScore():
             self.w = max(abs(self.scale_params[2] - self.b), abs(self.scale_params[3] - self.b))
             self.acc = 1.0 - abs(self.b - self.scale_params[0]) / self.w
 
-    def convert_to_score(self, value):
+    def convert_values_to_scores(self, values):
         # already in range 0, 1
         if len(self.scale_params) == 1:
-            return value  
+            return values
     
         # acceptable value, worst, best
         elif len(self.scale_params) == 3:
-            return (value - self.w) / (self.b - self.w)
+            return (values - self.w) / (self.b - self.w)
         
         # acceptable low, acceptable high, min value, max value
         elif len(self.scale_params) == 4:
-            return 1.0 - np.abs(self.b - value) / self.w
+            return 1.0 - np.abs(self.b - values) / self.w
         
-    def __call__(self, smiles):
-        out = self.predict(smiles)
-        if out:
-            return self.convert_to_score(out)
-        else:
-            return 0.0
+    def __call__(self, smiles_list):
+        """Make predictions for a given list of SMILES"""
+        values = self.predict(smiles_list)
 
-    def predict(self, smiles):
+        return self.convert_values_to_scores(values)
+
+    def predict(self, smiles_list):
         pass
 
 
 class CalcProp(ConvertScore):
-    """Scores structures based on calculated properties."""
+    """Scores structures based on calculated properties from RDKit."""
     def __init__(self, name, scale):
         super().__init__(scale_params=scale)
         
@@ -99,35 +100,22 @@ class CalcProp(ConvertScore):
             
             self.func = get_num_aromatic_rings
 
-    def predict(self, smiles):
-        mol = Chem.MolFromSmiles(smiles)
-        out = 0.0
-        if mol:
-            try: out = self.func(mol)
-            except: out = 0.0
+    def predict(self, smiles_list):
+        out = np.zeros(len(smiles_list))
+        for i, smi in enumerate(smiles_list):
+            mol = Chem.MolFromSmiles(smi)
+            out[i] = self.func(mol)
+
         return out
 
 
 class RFPredictor(ConvertScore):
     def __init__(self, model_path, scale=[0.5, 0., 1.]):
         super().__init__(scale_params=scale)
-
-        # self.model_path = model_path
-        with open(model_path, 'rb') as f:
-            self.rf = pkl.load(f)
+        self.model_path = model_path
     
-    def predict(self, smiles):
-        mol = Chem.MolFromSmiles(smiles)
-        out = None
-        if mol:
-            try:
-                ap_fp = calc_atompair_fp(smiles)
-                dp_fp = calc_donorpair_fp(smiles)
-                X = np.hstack([ap_fp, dp_fp])
-                # reshape to num samples x feature size
-                out = self.rf.predict(X.reshape(1, -1))[0]
-            except:
-                out = None
+    def predict(self, smiles_list):
+        out = predict_rf(smiles_list, self.model_path)
         return out
 
 
@@ -147,9 +135,9 @@ class MultiScore():
         self.names = names
         self.acclist = [sc.acc for sc in self.scorers]
 
-    # get total score for each smiles and fraction of smiles above acceptable threshold
-    def __call__(self, smiles):
-        scores = np.array([[s(smi) for s in self.scorers] for smi in smiles])
+    def __call__(self, smiles_list):
+        """Get total score for each SMILES and fraction of SMILES above acceptable threshold"""
+        scores = np.vstack([s(smiles_list) for s in self.scorers]).T
 
         # clip: negative values are set to 0; >1 set to 1
         scores = np.clip(scores, 0, 1)
@@ -166,7 +154,7 @@ class MultiScore():
         mean_tmp = np.mean(scores, axis=0)
         mint = mean_tmp - std_tmp
         maxt = mean_tmp + std_tmp
-        scores = (scores-mint) / (maxt-mint)
+        scores = (scores - mint) / (maxt - mint)
         scores = np.clip(scores, 0, 1)
 
         # scalarize scores for each compound
@@ -178,30 +166,13 @@ class MultiScore():
         #return np.sum(scores*weights, axis=1), frac
         return scalar_scores, frac
 
-    def score(self, smiles):
-        return [s(smiles) for s in self.scorers]
-
-    # get individual predictions
-    def predict(self, smiles):
-        return [s.predict(smiles) for s in self.scorers]
-
 
 class Scorer():
     def __init__(self, scoring_functions=None):
         self.scoring_functions = MultiScore(scoring_functions)
         self.names = self.scoring_functions.names
 
-    # get total weighted score for each smiles
-    def __call__(self, smiles):
-        scores, frac = self.scoring_functions(smiles)
+    def __call__(self, smiles_list):
+        """Get total weighted score for each smiles"""
+        scores, frac = self.scoring_functions(smiles_list)
         return scores, frac
-
-    # get individual scores for each scorer, without any reweighting
-    def score(self, smiles):
-        scores = [self.scoring_functions.score(smi) for smi in smiles]
-        return np.array(scores, dtype=np.float)
-
-    # get individual predictions for each predictor
-    def predict(self, smiles):
-        preds = [self.scoring_functions.predict(smi) for smi in smiles]
-        return np.array(preds, dtype=np.float)
